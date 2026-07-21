2e081c23dc5dec753f5c4975789132a13022edb5
cubis182 <matthew_dehass@yahoo.com>
2026-07-21 16:11:43 -0700

    added new page to data accuracy dashboard

--- functions.R
+++ functions.R
@@ -14,6 +14,33 @@
 
 # data-processing.Rmd -----------------------------------------------------
 
+# Replace ambiguous names
+replace_ambiguous <- function(data, use_parent) {
+  
+  # Replace ambiguous names (i.e. Imp can be Imperfective or Imperative)
+  replace_value <- function(column, to_replace, replace_string) {
+    # Rename "Imp" to "Imperative" in mood category
+    levels(data[[column]]) <- c(levels(data[[column]]), replace_string)
+    sequence <- which(data[[column]] == to_replace)
+    data[[column]][sequence] <- replace_string
+    remove(sequence)
+    
+    data[[column]][data[[column]] == to_replace] <- replace_string
+    if (is.factor(data[[column]])) data[[column]] <- droplevels(data[[column]]) # Remove the old "Imp", if a factor
+    return(data)
+  }
+  
+  data <- replace_value("Mood", "Imp", "Imperative")
+  data <- replace_value("Mood", "Ind", "Indicative")
+  if (use_parent) {
+    data <- replace_value("parent_Mood", "parent_Ind", "parent_Indicative")
+    data <- replace_value("parent_Mood", "parent_Imp", "parent_Imperative")
+  }
+  
+  data
+}
+
+
 # Cleans data from ./postagged folder by altering and renaming variables
 # in a way convenient for the analysis in data-processing.Rmd.
 # Does not modify in place, so the result must be assigned to the data frame
@@ -23,10 +50,6 @@
 prepare_data <- function(data, use_parent = params$use_parent) {
   require(magrittr)
   require(tidyverse)
-
-  # This isn't a ratio or count variable
-  data %<>%
-    mutate(Person = as.factor(Person))
 
   # T he Bellum Alexandrinum may represent two clear stages of composition,
   # and we might want to respond to Gaertner's argument that the completion
@@ -54,25 +77,7 @@ prepare_data <- function(data, use_parent = params$use_parent) {
     rowwise() %>%
     mutate(title = gsub(" ", "", paste(title, "_", book, "_", section)))
 
-  # Replace ambiguous names (i.e. Imp can be Imperfective or Imperative)
-  replace_value <- function(column, to_replace, replace_string) {
-    # Rename "Imp" to "Imperative" in mood category
-    levels(data[[column]]) <- c(levels(data[[column]]), replace_string)
-    sequence <- which(data[[column]] == to_replace)
-    data[[column]][sequence] <- replace_string
-    remove(sequence)
-
-    data[[column]][data[[column]] == to_replace] <- replace_string
-    data[[column]] <- droplevels(data[[column]]) # Remove the old "Imp
-    return(data)
-  }
-
-  data <- replace_value("Mood", "Imp", "Imperative")
-  data <- replace_value("Mood", "Ind", "Indicative")
-  if (use_parent) {
-    data <- replace_value("parent_Mood", "parent_Ind", "parent_Indicative")
-    data <- replace_value("parent_Mood", "parent_Imp", "parent_Imperative")
-  }
+  data %<>% replace_ambiguous(use_parent)
 
   data
 }
